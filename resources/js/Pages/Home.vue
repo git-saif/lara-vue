c958d4bb7496fe370f1b5f20a25c539e581830aa
git-saif <saifse454@gmail.com>
2025-11-06 03:12:47 +0600

    bootstrap installed

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>🏠 Home Page</h2>
-    <p>Welcome to Vue Router Demo inside Laravel!</p>
+    <h2 class="text-center text-danger fs-1">🏠 Home Page</h2>
+    <p class="text-center">Welcome to Vue Router Demo inside Laravel!</p>
   </div>
 </template>

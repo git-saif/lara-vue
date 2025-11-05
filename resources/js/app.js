61df24c114d978f30b9205ef8e92845121944dd0
git-saif <saifse454@gmail.com>
2025-11-06 01:40:29 +0600

    updated router path

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,7 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/routes';
 
 createApp(App)
   .use(router)
